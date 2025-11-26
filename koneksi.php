cc61cbedf3609b33ece1e4741d950a98daeec724
Lyzn <58847651+Lyzn7@users.noreply.github.com>
2025-11-26 18:54:03 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 $host = getenv('DB_HOST') ?: 'localhost';
-$user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASS') ?: '';
-$db   = getenv('DB_NAME') ?: 'monitor_keuangan';
-$port = getenv('DB_PORT') ?: 3306;
+$user = getenv('DB_USER') ?: 'maglev.proxy.rlwy.net';
+$pass = getenv('DB_PASS') ?: 'qKHVsZjpnrmMKuKeDFujKcEnbnuEnXMk';
+$db   = getenv('DB_NAME') ?: 'railway';
+$port = getenv('DB_PORT') ?: 26932 ;                     ;
 
 $k = new mysqli($host, $user, $pass, $db, $port);
 
